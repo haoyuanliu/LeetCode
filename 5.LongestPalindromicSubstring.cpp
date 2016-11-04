73d33143351f71e570605c8ad2af835c1312d9a4
zuilehongdou <15120101@bjtu.edu.cn>
2016-11-04 22:53:51 +0800

    add pro5 solution

--- 5.LongestPalindromicSubstring.cpp
+++ 5.LongestPalindromicSubstring.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 class Solution
@@ -6,11 +7,41 @@ class Solution
     public:
         string longestPalindrome(string s)
         {
-
+            int p, q, max;
+            int start, end;
+            p = q = max = 0;
+            string str = '&' + s;
+            int len = str.length();
+            for(int i = 1; i < len; ++i)
+            {
+                p = i;
+                while(s[i] == s[i+1])
+                    i++;
+                q = i;
+                while(s[p-1] == s[q+1])
+                {
+                    p--;
+                    q++;
+                }
+                if(max < (q-p+1))
+                {
+                    start = p;
+                    end = q;
+                    max = q-p+1;
+                }
+            }
+            str = str.substr(start+1, max);
+            return str;
         }
 };
 
 int main()
 {
+    string str;
+    Solution res;
+    while(cin >> str)
+    {
+        cout << res.longestPalindrome(str) << endl;
+    }
     return 0;
 }
