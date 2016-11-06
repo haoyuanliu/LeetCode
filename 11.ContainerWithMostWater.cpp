788dc2294f4d41d21a70b42073dabf4c38a0903e
zuilehongdou <15120101@bjtu.edu.cn>
2016-11-06 19:28:32 +0800

    add pro11 solution

--- 11.ContainerWithMostWater.cpp
+++ 11.ContainerWithMostWater.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 class Solution
@@ -6,11 +7,29 @@ class Solution
     public:
         int maxArea(vector<int>& height)
         {
-
+            int res = 0;
+            int len = height.size();
+            int l = 0;
+            int r = len - 1;
+            while(l < r)
+            {
+                res = max(res, (r-l)*min(height[l], height[r]));
+                if(height[l] < height[r])
+                    l++;
+                else
+                    r--;
+            }
+            return res;
         }
 };
 
 int main()
 {
+    vector<int> n;
+    int num;
+    while(cin >> num)
+        n.push_back(num);
+    Solution res;
+    cout << res.maxArea(n) << endl;
     return 0;
 }
