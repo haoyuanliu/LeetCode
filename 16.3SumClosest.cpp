8bc640e72937fb31cf62d54ad9a83ccb186674c0
zuilehongdou <15120101@bjtu.edu.cn>
2016-11-07 21:44:07 +0800

    add pro16 solution

--- 16.3SumClosest.cpp
+++ 16.3SumClosest.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 class Solution
@@ -7,7 +8,36 @@ class Solution
     public:
         int threeSumClosest(vector<int>& nums, int target)
         {
-
+            if(nums.size() < 3)
+                return 0;
+            if(nums.size() == 3)
+                return nums[0]+nums[1]+nums[2];
+            int res = (0x7fffffff)/2;
+            sort(nums.begin(), nums.end());
+            for(int i = 0; i < nums.size()-2; ++i)
+            {
+                int l = i+1;
+                int r = nums.size()-1;
+                while(l < r)
+                {
+                    int temp = nums[i] + nums[l] + nums[r];
+                    if(l < r && temp < target)
+                    {
+                        if(abs(res - target) > abs(temp - target))
+                            res = temp;
+                        l++;
+                    }
+                    else if(l < r && temp > target)
+                    {
+                        if(abs(res - target) > abs(temp - target))
+                            res = temp;
+                        r--;
+                    }
+                    else
+                        return target;
+                }
+            }
+            return res;
         }
 };
 
