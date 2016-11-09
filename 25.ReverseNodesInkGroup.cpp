c8e2285b935e519706ca7c160875ec6109b8cbc2
zuilehongdou <15120101@bjtu.edu.cn>
2016-11-09 16:54:44 +0800

    add pro25 solution

--- 25.ReverseNodesInkGroup.cpp
+++ 25.ReverseNodesInkGroup.cpp
@@ -13,7 +13,43 @@ class Solution
     public:
         ListNode* reverseKGroup(ListNode* head, int k)
         {
-            
+            int len = getLen(head);
+            if(len < k || k == 1)
+                return head;
+            ListNode *root = new ListNode(-1);
+            ListNode *p = root;
+            ListNode *p1 = head;
+            ListNode *p2 = head;
+            while(len >= k)
+            {
+                for(int i = 0; i < k; ++i)
+                {
+                    p2 = p1->next;
+                    p1->next = p->next;
+                    p->next = p1;
+                    p1 = p2;
+                }
+                while(p->next)
+                    p = p->next;
+                len -= k;
+                if(len < k)
+                {
+                    p->next = p1;
+                    return root->next;
+                }
+            }
+            return root->next;
+        }
+        int getLen(ListNode *head)
+        {
+            ListNode *p = head;
+            int count = 0;
+            while(p)
+            {
+                count++;
+                p = p->next;
+            }
+            return count;
         }
 };
 
