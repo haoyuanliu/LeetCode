6aea0d51088cfed458820f571a50c06e621896fa
zuilehongdou <15120101@bjtu.edu.cn>
2016-11-09 23:01:18 +0800

    add pro30 solution not accepted

---
+++ 30.SubstringWithConcatenationOfAllWords.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+class Solution
+{
+    public:
+        vector<int> findSubstring(string s, vector<string> &words)
+        {
+
+        }
+};
+
+int main()
+{
+    return 0;
+}
