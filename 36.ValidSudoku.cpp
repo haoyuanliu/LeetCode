24c8310b2177f2217ac63d068cd73a731d496c61
haoyuanliu <15120101@bjtu.edu.cn>
2016-11-14 23:57:31 +0800

    add pro36 but not accepted

---
+++ 36.ValidSudoku.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution
+{
+    public:
+        bool isValidSudoku(vector<vector<char> >& board)
+        {
+
+        }
+};
+
+int main()
+{
+    return 0;
+}
