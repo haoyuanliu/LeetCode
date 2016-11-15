8a0334c43fd536a7e7adff3c8d87696d014d5f5b
zuilehongdou <15120101@bjtu.edu.cn>
2016-11-15 09:25:49 +0800

    add pro36 solution

--- 36.ValidSudoku.cpp
+++ 36.ValidSudoku.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <string.h>
 using namespace std;
 
 class Solution
@@ -7,7 +8,54 @@ class Solution
     public:
         bool isValidSudoku(vector<vector<char> >& board)
         {
-
+            int used[10];
+            if(board.size() != 9 || board[0].size() != 9)
+                return false;
+            for(int i = 0; i < 9; ++i)
+            {
+                memset(used, 0, sizeof(used));
+                for(int j = 0; j < 9; ++j)
+                {
+                    if(board[i][j] == '.')
+                        continue;
+                    if(board[i][j] <= '0' || board[i][j] > '9')
+                        return false;
+                    if(used[board[i][j] - '0'])
+                        return false;
+                    used[board[i][j] - '0'] = 1;
+                }
+            }
+            for(int j = 0; j < 9; ++j)
+            {
+                memset(used, 0, sizeof(used));
+                for(int i = 0; i < 9; ++i)
+                {
+                    if(board[i][j] == '.')
+                        continue;
+                    if(used[board[i][j] - '0'])
+                        return false;
+                    used[board[i][j] - '0'] = 1;
+                }
+            }
+            for(int i = 0; i < 9; i += 3)
+            {
+                for(int j = 0; j < 9; j += 3)
+                {
+                    memset(used, 0, sizeof(used));
+                    for(int k = i; k < i+3; ++k)
+                    {
+                        for(int l = j; l < j+3; ++l)
+                        {
+                            if(board[k][l] == '.')
+                                continue;
+                            if(used[board[k][l] - '0'])
+                                return false;
+                            used[board[k][l] - '0'] = 1;
+                        }
+                    }
+                }
+            }
+            return true;
         }
 };
 
