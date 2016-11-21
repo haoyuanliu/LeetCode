7756e66f167aad5a0dc9ec12bac2aa368cd732a7
zuilehongdou <15120101@bjtu.edu.cn>
2016-11-21 17:42:34 +0800

    add pro40 solution

--- 40.CombinationSumII.cpp
+++ 40.CombinationSumII.cpp
@@ -8,7 +8,30 @@ class Solution
     public:
         vector<vector<int> > combinationSum2(vector<int> &candidates, int target)
         {
-
+            vector<vector<int> > res;
+            vector<int> temp;
+            sort(candidates.begin(), candidates.end());
+            solve(res, temp, candidates, target, 0, 0, candidates.size());
+            return res;
+        }
+        void solve(vector<vector<int> > &res, vector<int> &temp, vector<int> candidates, int target, int cur, int sum, int len)
+        {
+            if(sum == target)
+            {
+                res.push_back(temp);
+                return;
+            }
+            for(int i = cur; i < len; ++i)
+            {
+                if(sum + candidates[i] <= target)
+                {
+                    temp.push_back(candidates[i]);
+                    solve(res, temp, candidates, target, i+1, sum+candidates[i], len);
+                    temp.pop_back();
+                    while(i+1 < len && candidates[i] == candidates[i+1]) //如果下一个值等于当前值，则跳过，
+                        i++;
+                }
+            }
         }
 };
 
