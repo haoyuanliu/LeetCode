f62be3ee9d1952d6882f4c8ed519183715403d3e
zuilehongdou <15120101@bjtu.edu.cn>
2016-11-21 15:31:23 +0800

    pro39 but not accepted

--- 39.CombinationSum.cpp
+++ 39.CombinationSum.cpp
@@ -15,8 +15,27 @@ class Solution
             sort(candidates.begin(), candidates.end());
             for(it = candidates.begin(); it != candidates.end(); ++it)
             {
-                if(*it == *(it+1) && it+1 != candidates.end())
+                while(*it == *(it+1) && it+1 != candidates.end())
                     it = candidates.erase(it);
+            }
+            solve(temp, candidates, res, target, 0, candidates.size(), 0);
+            return res;
+        }
+        void solve(vector<int> &temp, vector<int> candidates, vector<vector<int> > &res, int target, int cur, int len, int sum)
+        {
+            if(sum == target)
+            {
+                res.push_back(temp);
+                return;
+            }
+            for(int i = cur; i < len; ++i)
+            {
+                if(sum + candidates[i] <= target)
+                {
+                    temp.push_back(candidates[i]);
+                    solve(temp, candidates, res, target, i, len, sum + candidates[i]);
+                    temp.pop_back();
+                }
             }
         }
 };
