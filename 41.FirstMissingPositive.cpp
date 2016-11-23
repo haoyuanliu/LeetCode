5ca4f0aa21a15597d502d5be9261cc7d6240839f
zuilehongdou <15120101@bjtu.edu.cn>
2016-11-23 09:52:34 +0800

    add pro41 solutions

--- 41.FirstMissingPositive.cpp
+++ 41.FirstMissingPositive.cpp
@@ -4,6 +4,7 @@
 #include <algorithm>
 using namespace std;
 
+//Solution 1 6ms
 class Solution
 {
     public:
@@ -23,6 +24,51 @@ class Solution
                     return res;
             }
             return res;
+        }
+};
+
+
+//Solution 2 3ms
+class Solution
+{
+    public:
+        int firstMissingPositive(vector<int> &nums)
+        {
+            int len = nums.size();
+            int temp[len];
+            for(int i = 0; i < len; ++i)
+            {
+                if(nums[i] > 0 && nums[i] <= len)
+                    temp[nums[i] - 1] = nums[i];
+                else
+                    continue;
+            }
+
+            for(int i = 0; i < len; ++i)
+                if(temp[i] != i+1)
+                    return i+1;
+            return len+1;
+        }
+};
+
+//Solution 3 3ms
+class Solution
+{
+    public:
+        int firstMissingPositive(vector<int> &nums)
+        {
+            int len = nums.size();
+            for(int i = 0; i < len; )
+            {
+                if(nums[i] > 0 && nums[i] <= len && nums[i] != nums[nums[i] - 1])
+                    swap(nums[i], nums[nums[i] -1]);
+                else
+                    i++;
+            }
+            for(int i = 0; i < len; ++i)
+                if(nums[i] != i+1)
+                    return i+1;
+            return len+1;
         }
 };
 
