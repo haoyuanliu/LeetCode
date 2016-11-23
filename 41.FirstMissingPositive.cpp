7695cb9fb5af93de57567b6e2e88941c2de10e40
zuilehongdou <15120101@bjtu.edu.cn>
2016-11-23 09:26:38 +0800

    add pro41 solution accepted

--- 41.FirstMissingPositive.cpp
+++ 41.FirstMissingPositive.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <map>
+#include <algorithm>
 using namespace std;
 
 class Solution
@@ -7,7 +9,20 @@ class Solution
     public:
         int firstMissingPositive(vector<int>& nums)
         {
-
+            int res = 1;
+            sort(nums.begin(), nums.end());
+            for(int i = 0; i < nums.size(); ++i)
+            {
+                if(nums[i] <= 0)
+                    continue;
+                if(nums[i] == res-1)
+                    continue;
+                if(nums[i] == res)
+                    res = nums[i]+1;
+                else
+                    return res;
+            }
+            return res;
         }
 };
 
