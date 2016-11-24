ef0c6312f51f3065d2c882d550d113ce1d2a3cc4
zuilehongdou <15120101@bjtu.edu.cn>
2016-11-24 23:24:03 +0800

    add pro44 solution

--- 44.WildcardMatching.cpp
+++ 44.WildcardMatching.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include <vector>
 #include <string>
+#include <algorithm>
 using namespace std;
 
 class Solution
@@ -7,6 +9,34 @@ class Solution
     public:
         bool isMatch(string s, string p)
         {
+            int m = s.size();
+            int n = p.size();
+            int cnt = count(p.begin(), p.end(), '*');
+            if(n - cnt > m)
+                return false;
+            vector<bool> dp(n+1, false);
+            dp[0] = 1;
+            for(int j = 1; j <= n; ++j)
+                if(p[j-1] == '*')
+                    dp[j] = dp[j-1];
+            for(int i = 1; i <= m; ++i)
+            {
+                vector<bool> cur(n+1, false);
+                for(int j = 1; j <= n; j++)
+                {
+                    if(p[j-1] == '*')
+                    {
+                        cur[j] = dp[j] || dp[j-1];
+                    }
+                    else
+                    {
+                        if(s[i-1] == p[j-1] || p[j-1] == '?')
+                            cur[j] = dp[j-1];
+                    }
+                }
+                dp = cur;
+            }
+            return dp[n];
 
         }
 };
