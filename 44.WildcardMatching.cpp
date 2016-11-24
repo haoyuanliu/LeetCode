8e039c8b54bca7cf8f5cd1021233099e1761786f
zuilehongdou <15120101@bjtu.edu.cn>
2016-11-25 01:06:04 +0800

    add pro44 solution

--- 44.WildcardMatching.cpp
+++ 44.WildcardMatching.cpp
@@ -4,7 +4,7 @@
 #include <algorithm>
 using namespace std;
 
-class Solution
+/*class Solution
 {
     public:
         bool isMatch(string s, string p)
@@ -26,7 +26,7 @@ class Solution
                 {
                     if(p[j-1] == '*')
                     {
-                        cur[j] = dp[j] || dp[j-1];
+                        cur[j] = dp[j] || cur[j-1];
                     }
                     else
                     {
@@ -38,6 +38,40 @@ class Solution
             }
             return dp[n];
 
+        }
+};*/
+
+class Solution
+{
+    public:
+        bool isMatch(string s, string p)
+        {
+            int m = s.size();
+            int n = p.size();
+            int cnt = count(p.begin(), p.end(), '*');
+            if(n - cnt > m)
+                return false;
+            vector<bool> dp(n+1, false);
+            dp[0] = 1;
+            for(int i = 0; i < n; ++i)
+                if(p[i] == '*')
+                    dp[i+1] = dp[i];
+            for(int i = 1; i <= m; ++i)
+            {
+                vector<bool> cur(n+1, false);
+                for(int j = 1; j <= n; ++j)
+                {
+                    if(p[j-1] == '*')
+                        cur[j] = cur[j-1] || dp[j];
+                    else
+                    {
+                        if(p[j-1] == s[i-1] || p[j-1] == '?')
+                            cur[j] = dp[j-1];
+                    }
+                }
+                dp = cur;
+            }
+            return dp[n];
         }
 };
 
