c40a99d4870b3a69ba6d8eaed317eb42854501ff
zuilehongdou <15120101@bjtu.edu.cn>
2016-11-25 01:09:03 +0800

    add pro55

---
+++ 55.JumpGameII.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution
+{
+    public:
+        int jump(vector<int> &nums)
+        {
+
+        }
+};
+
+int main()
+{
+    Solution demo;
+    int temp;
+    vector<int> nums;
+    while(cin >> temp)
+        nums.push_back(temp);
+    cout << demo.jump(nums) << endl;
+    return 0;
+}
