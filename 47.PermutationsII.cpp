17f2532d0ee9ab2e92168bc58e73c8988f62cd71
zuilehongdou <15120101@bjtu.edu.cn>
2016-11-28 22:35:22 +0800

    add pro47 solution but not accepted

---
+++ 47.PermutationsII.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+
+class Solution
+{
+    public:
+        vector<vector<int> > permuteUnique(vector<int> & nums)
+        {
+
+        }
+};
+
+int main()
+{
+    return 0;
+}
