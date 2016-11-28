d5ff2d4d3aa5b5160232053d3c16a232b073a0cf
zuilehongdou <15120101@bjtu.edu.cn>
2016-11-28 20:39:55 +0800

    add pro55 solution

---
+++ 55.JumpGame.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution
+{
+    public:
+        bool canJump(vector<int> &nums)
+        {
+            int len = nums.size();
+            if(len == 1)
+                return true;
+            int maxJump = 0;
+            for(int i = 0; i < len; ++i)
+            {
+                maxJump--;
+                if(maxJump < nums[i])
+                    maxJump = nums[i];
+
+                if(!maxJump)
+                    return false;
+                if(maxJump + i >= len - 1)
+                    return true;
+            }
+            return true;
+        }
+};
+
+int main()
+{
+    Solution demo;
+    int temp;
+    vector<int> nums;
+    while(cin >> temp)
+        nums.push_back(temp);
+    if(demo.canJump(nums))
+        cout << "Yes" << endl;
+    else
+        cout << "No" << endl;
+    return 0;
+}
