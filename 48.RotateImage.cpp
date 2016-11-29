05d3ce36be269ea60a63e514e13791c1ecba1213
zuilehongdou <15120101@bjtu.edu.cn>
2016-11-29 10:03:28 +0800

    update

---
+++ 48.RotateImage.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution
+{
+    public:
+        void rotate(vector<vector<int> > &matrix)
+        {
+            int row = matrix.size();
+            int col = matrix[0].size();
+            vector<vector<int> > res;
+            vector<int> temp;
+            for(int i = 0; i < col; ++i)
+            {
+                temp.clear();
+                for(int j = row-1; j >= 0; --j)
+                {
+                    temp.push_back(matrix[j][i]);
+                }
+                res.push_back(temp);
+            }
+            matrix = res;
+        }
+};
+
+class Solution
+{
+    public:
+        void rotate(vector<vector<int> > &matrix)
+        {
+            int size = matrix.size();
+            for(int i = 0; i < size; ++i)
+                for(int j = 0; j < size - i; ++j)
+                    swap(matrix[i][j], matrix[size-1-i][size-1-j]);
+            reverse(matrix.begin(), matrix.end());
+        }
+};
+
+int main()
+{
+    return 0;
+}
