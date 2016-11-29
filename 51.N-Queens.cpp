08037365dbe6754c4f656e339b1e9697748ac781
zuilehongdou <15120101@bjtu.edu.cn>
2016-11-29 12:38:30 +0800

    add pro51 solution but not accepted

---
+++ 51.N-Queens.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution
+{
+    public:
+        vector<vector<string> > solveNQueens(int n)
+        {
+
+        }
+};
+
+int main()
+{
+    return 0;
+}
