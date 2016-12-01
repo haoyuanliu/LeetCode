f706ec8c8eddcaaf68dcba7d9957823c52e6521f
zuilehongdou <15120101@bjtu.edu.cn>
2016-12-01 10:06:09 +0800

    update pro51 solution

--- 51.N-Queens.cpp
+++ 51.N-Queens.cpp
@@ -3,6 +3,7 @@
 #include <cmath>
 using namespace std;
 
+//Solution I 9ms
 class Solution
 {
     public:
@@ -38,6 +39,54 @@ class Solution
             }
         }
         bool place(int k)
+        {
+            for(int i = 0; i < k; ++i)
+            {
+                if(abs(x[k] - x[i]) == abs(k - i) || x[k] == x[i])
+                    return false;
+            }
+            return true;
+        }
+};
+
+//Solution II 6ms
+class Solution
+{
+    public:
+        vector<vector<string> > solveNQueens(int n)
+        {
+            int x[n];
+            for(int i = 0; i < n; ++i)
+                x[i] = 0;
+            vector<vector<string> > res;
+            put_queen(res, x, 0, n);
+            return res;
+        }
+        void put_queen(vector<vector<string> > &res, int* x, int k, int n)
+        {
+            if(k >= n)
+            {
+                vector<string> str;
+                for(int i = 0; i < n; ++i)
+                {
+                    string temp(n, '.');
+                    temp[x[i]] = 'Q';
+                    str.push_back(temp);
+                }
+                res.push_back(str);
+            }
+            else
+            {
+                for(int i = 0; i < n; ++i)
+                {
+                    x[k] = i;
+                    if(place(x, k))
+                        put_queen(res, x, k+1, n);
+                    x[k] = 0;
+                }
+            }
+        }
+        bool place(int* x, int k)
         {
             for(int i = 0; i < k; ++i)
             {
