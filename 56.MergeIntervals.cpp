a35a954f947574948d811acc275e97f0a3d29529
zuilehongdou <15120101@bjtu.edu.cn>
2016-12-04 22:52:58 +0800

    add pro56 solution but not accepted

--- 56.MergeIntervals.cpp
+++ 56.MergeIntervals.cpp
@@ -11,6 +11,13 @@ struct Interval
     Interval(int s, int e) : start(s), end(e) {}
 };
 
+bool compare(pair<int, int> a, pair<int, int> b)
+{
+    if(a.first == b.first)
+        return abs(a.second) > abs(b.second);
+    else
+        return a.first < b.first;
+}
 class Solution
 {
     public:
@@ -24,6 +31,10 @@ class Solution
                 temp.push_back(make_pair(intervals[i].start, i+1));
                 temp.push_back(make_pair(intervals[i].end, -(i+1)));
             }
+            sort(temp.begin(), temp.end(), compare);
+            for(const auto x : temp)
+                cout << x.first << " " << x.second << endl;
+            cout << endl;
             int count = 0;
             int l, r;
             for(int i = 0; i < temp.size(); ++i)
@@ -32,13 +43,26 @@ class Solution
                     l = temp[i].first;
                 count += temp[i].second;
                 if(count == 0)
+                {
                     r = temp[i].first;
-                res.push_back(Interval(l, r));
+                    res.push_back(Interval(l, r));
+                }
             }
+            return res;
         }
 };
 
 int main()
 {
+    Solution demo;
+    int a, b;
+    vector<Interval> inter;
+    while(cin >> a >> b)
+    {
+        inter.push_back(Interval(a, b));
+    }
+    vector<Interval> res = demo.merge(inter);
+    for(const auto x : res)
+        cout << x.start << " " << x.end << endl;
     return 0;
 }
