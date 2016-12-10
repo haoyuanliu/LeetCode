71bf5e823bb06cb03d81586d376adfd77c9818f1
zuilehongdou <15120101@bjtu.edu.cn>
2016-12-10 21:23:48 +0800

    update pro62 solution

--- 62.UniquePaths.cpp
+++ 62.UniquePaths.cpp
@@ -31,10 +31,7 @@ class Solution
     public:
         int uniquePaths(int m, int n)
         {
-            int res[m+1][n+1];
-            for(int i = 0; i <= m; ++i)
-                for(int j = 0; j <= n; ++j)
-                    res[i][j] = 0;
+            int res[101][101] = {0};
             for(int i = 1; i <= m; ++i)
                 res[i][1] = 1;
             for(int j = 1; j <= n; ++j)
@@ -55,16 +52,13 @@ class Solution
     public:
         int uniquePaths(int m, int n)
         {
-            int res[2][n+1];
-            for(int i = 0; i <= n; ++i)
-                res[0][i] = 1;
-            for(int i = 1; i < m; ++i)
+            int res[2][101] = {0};
+            for(int i = 1; i <= n; ++i)
+                res[0][i] = res[1][i] = 1;
+            for(int i = 2; i <= m; ++i)
             {
                 for(int j = 1; j <= n; ++j)
                 {
-                    if(j == 1)
-                        res[1][j] = 1;
-                    else
                         res[1][j] = res[0][j] + res[1][j-1];
                 }
                 for(int k = 0; k <= n; ++k)
