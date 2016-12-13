c6d2ecdfef0e0bb2ef8164ab9087df4aa49d9de3
zuilehongdou <15120101@bjtu.edu.cn>
2016-12-13 23:52:20 +0800

    add pro67 solution

--- 67.AddBinary.cpp
+++ 67.AddBinary.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <algorithm>
 using namespace std;
 
 class Solution
@@ -7,11 +8,38 @@ class Solution
     public:
         string addBinary(string a, string b)
         {
-
+            int m = a.size();
+            int n = b.size();
+            reverse(a.begin(), a.end());
+            reverse(b.begin(), b.end());
+            int carry = 0;
+            string res = "";
+            int x, y;
+            for(int i = 0; i < max(m, n); ++i)
+            {
+                if(i >= m)
+                    x = 0;
+                else
+                    x = a[i] - '0';
+                if(i >= n)
+                    y = 0;
+                else
+                    y = b[i] - '0';
+                res += ((x + y + carry) % 2 + '0');
+                carry = (x + y + carry) / 2;
+            }
+            if(carry)
+                res += '1';
+            reverse(res.begin(), res.end());
+            return res;
         }
 };
 
 int main()
 {
+    Solution demo;
+    string s1, s2;
+    while(cin >> s1 >> s2)
+        cout << demo.addBinary(s1, s2) << endl;
     return 0;
 }
