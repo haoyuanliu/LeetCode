d318640d3d90315371103b9167952aadee6e0445
zuilehongdou <15120101@bjtu.edu.cn>
2016-12-13 11:17:02 +0800

    update pro174 solution

--- 174.DungeonGame.cpp
+++ 174.DungeonGame.cpp
@@ -11,20 +11,12 @@ class Solution
             int n = dumgeon[0].size();
             dumgeon[m-1][n-1] = max(1, 1 - dumgeon[m-1][n-1]);
             for(int j = n-2; j >= 0; --j)
-            {
                 dumgeon[m-1][j] = max(1, dumgeon[m-1][j+1] - dumgeon[m-1][j]);
-            }
             for(int i = m-2; i >= 0; --i)
-            {
                 dumgeon[i][n-1] = max(1, dumgeon[i+1][n-1] - dumgeon[i][n-1]);
-            }
             for(int i = m-2; i >= 0; --i)
-            {
                 for(int j = n-2; j >= 0; --j)
-                {
                     dumgeon[i][j] = max(1, min(dumgeon[i+1][j], dumgeon[i][j+1]) - dumgeon[i][j]);
-                }
-            }
             return dumgeon[0][0];
         }
 };
