dd07539558ef0ec3e975d9619534d9ec32134b5a
zuilehongdou <15120101@bjtu.edu.cn>
2016-12-18 15:20:06 +0800

    update pro215 solution

--- 215.KthLargestElementinanArray.cpp
+++ 215.KthLargestElementinanArray.cpp
@@ -13,6 +13,45 @@ class Solution
         }
 };
 
+class Solution
+{
+    public:
+        int findKthLargest(vector<int> &nums, int k)
+        {
+            int len = nums.size();
+            int res = 0;
+            int left = 0;
+            int right = len;
+            while(left < right)
+            {
+                int pos = partition(nums, left, right);
+                if(pos == len-k)
+                {
+                    res = nums[pos];
+                    break;
+                }
+                else if(pos < len-k)
+                    left = pos+1;
+                else
+                    right = pos;
+            }
+            return res;
+        }
+        int partition(vector<int> &nums, int begin, int end)
+        {
+            int pivot = nums[begin];
+            while(begin < end)
+            {
+                while(begin < end && nums[--end] >= pivot);
+                nums[begin] = nums[end];
+                while(begin < end && nums[++begin] <= pivot);
+                nums[end] = nums[begin];
+            }
+            nums[begin] = pivot;
+            return begin;
+        }
+};
+
 int main()
 {
     return 0;
