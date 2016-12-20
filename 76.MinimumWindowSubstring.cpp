6c88a10122da978a83f52f3958792f84c1b51390
zuilehongdou <15120101@bjtu.edu.cn>
2016-12-20 19:03:06 +0800

    add pro76 solution

--- 76.MinimumWindowSubstring.cpp
+++ 76.MinimumWindowSubstring.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+#include <map>
 #include <string>
 using namespace std;
 
@@ -7,7 +9,29 @@ class Solution
     public:
         string minWindow(string s, string t)
         {
-
+            vector<int> ch(128, 0);
+            for(int i = 0; i < t.size(); ++i)
+                ch[t[i]]++;
+            int count = t.size();
+            int begin, end, head, resLen;
+            begin = end = head = 0;
+            resLen = s.size() + 1;
+            while(end < s.size())
+            {
+                if(ch[s[end++]]-- > 0)
+                    count--;
+                while(count == 0)
+                {
+                    if(end - begin < resLen)
+                    {
+                        resLen = end - begin;
+                        head = begin;
+                    }
+                    if(ch[s[begin++]]++ == 0)
+                        count++;
+                }
+            }
+            return resLen==s.size()+1 ? "" : s.substr(head, resLen);
         }
 };
 
