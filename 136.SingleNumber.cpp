a6716bad774b5b3d38a159b4af2cc38b51c5e706
zuilehongdou <15120101@bjtu.edu.cn>
2016-12-23 09:39:10 +0800

    add pro136 solution

--- 136.SingleNumber.cpp
+++ 136.SingleNumber.cpp
@@ -29,6 +29,20 @@ class Solution
         }
 };
 
+class Solution
+{
+    public:
+        int singleNumber(vector<int> &nums)
+        {
+            int res = 0;
+            for(int i = 0; i < nums.size(); i++)
+            {
+                res ^= nums[i];
+            }
+            return res;
+        }
+};
+
 int main()
 {
     return 0;
