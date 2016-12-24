f24edf9c339970c593b902322af16c4f6c3eb786
zuilehongdou <15120101@bjtu.edu.cn>
2016-12-24 16:59:43 +0800

    add pro 268 solution

--- 268.MissingNumber.cpp
+++ 268.MissingNumber.cpp
@@ -38,6 +38,41 @@ class Solution
         }
 };
 
+class Solution
+{
+    public:
+        int missingNumber(vector<int> &nums)
+        {
+            int res = 0;
+            int len = nums.size();
+            res = len * (len + 1) / 2;
+            for(int i = 0; i < len; ++i)
+                res -= nums[i];
+            return res;
+        }
+};
+
+class Solution
+{
+    public:
+        int missingNumber(vector<int> &nums)
+        {
+            sort(nums.begin(), nums.end());
+            int left = 0;
+            int right = nums.size();
+            int mid = (left + right) / 2;
+            while(left < right)
+            {
+                mid = (left + right) / 2;
+                if(mid < nums[mid])
+                    right = mid;
+                else
+                    left = mid+1;
+            }
+            return left;
+        }
+};
+
 int main()
 {
     return 0;
