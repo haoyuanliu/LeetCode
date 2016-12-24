ecd3f3e30c72d65cbddc962a52ffbdc4f11b7d17
zuilehongdou <15120101@bjtu.edu.cn>
2016-12-24 22:35:46 +0800

    add pro393 solution but not accepted

---
+++ 393.UTF-8Validation.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution
+{
+    public:
+        bool validUtf8(vector<int> &data)
+        {
+
+        }
+};
+
+int main()
+{
+    return 0;
+}
