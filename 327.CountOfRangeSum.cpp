1bbc9ee03ccc8b011bf0a293400966e9a8d3d5cc
zuilehongdou <15120101@bjtu.edu.cn>
2016-12-28 14:52:25 +0800

    add pro327 solution

--- 327.CountOfRangeSum.cpp
+++ 327.CountOfRangeSum.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 using namespace std;
 
+
 class Solution
 {
 public:
@@ -42,7 +43,54 @@ public:
     }
 };
 
+class Solution
+{
+    public:
+        int countRangeSum(vector<int>& nums, int lower, int upper) 
+        {
+            int len = nums.size();
+            if(len == 0)
+                return 0;
+            vector<long> sums(len+1, 0);
+            for(int i = 0; i < len; ++i)
+                sums[i+1] = sums[i] + nums[i];
+            vector<long> sumUpdate = sums;
+            return solve(sums, sumUpdate, 0, len+1, lower, upper);
+        }
+        int solve(vector<long> &sums, vector<long> &sumUpdate, int start, int end, int lower, int upper)
+        {
+            if(end - start <= 1)
+                return 0;
+            int mid = (end + start) / 2;
+            int count = solve(sums, sumUpdate, start, mid, lower, upper)
+                + solve(sums, sumUpdate, mid, end, lower, upper);
+            int l = mid;
+            int r = mid;
+            int t = mid;
+            int s = start;
+            for(int i = start; i < mid; ++i)
+            {
+                while(sums[l] - sums[i] < lower && l < end)
+                    l++;
+                while(sums[r] - sums[i] <= upper && r < end)
+                    r++;
+                while(sums[i] > sums[t] && t < end)
+                    sumUpdate[s++] = sums[t++];
+                sumUpdate[s++] = sums[i];
+                count += r - l;
+            }
+            for(int i = start; i < end; ++i)
+                sums[i] = sumUpdate[i];
+            return count;
+        }
+};
+
 int main()
 {
+    Solution demo;
+    int lower = -2;
+    int upper = 2;
+    vector<int> nums= {-2, 5, -1};
+    cout << demo.countRangeSum(nums, lower, upper) << endl;
     return 0;
 }
