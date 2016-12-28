1a79153723a5d1f1b84a47ba5e6d7ee55810d484
zuilehongdou <15120101@bjtu.edu.cn>
2016-12-28 10:32:09 +0800

    update pro315 solution

--- 315.CountOfSmallerNumbersAfterSelf.cpp
+++ 315.CountOfSmallerNumbersAfterSelf.cpp
@@ -57,6 +57,96 @@ class Solution
         }
 };
 
+bool cmp(const pair<int, int> a, const pair<int, int> b)
+{
+    return a.first < b.first; 
+}
+
+class Solution
+{
+    public:
+        vector<int> countSmaller(vector<int> &nums)
+        {
+            int len = nums.size();
+            vector<int> res(len, 0);
+            vector<pair<int, int>> temp;
+            for(int i = 0; i < len; ++i)
+                temp.push_back(make_pair(nums[i], i));
+            solve(res, temp, 0, len);
+            return res;
+        }
+        void solve(vector<int> &res, vector<pair<int, int>> &temp, int start, int end)
+        {
+            if(end - start <= 1)
+                return;
+            int mid = (start + end) / 2;
+            solve(res, temp, mid, end);
+            solve(res, temp, start, mid);
+            int r = mid;
+            for(int l = start; l < mid; ++l)
+            {
+                while(temp[l].first > temp[r].first && r < end)
+                    r++;
+                res[temp[l].second] += r - mid;
+            }
+            sort(temp.begin() + start, temp.begin() + end, cmp);
+        }
+};
+
+
+bool cmp(const pair<int, int> a, const pair<int, int> b)
+{
+    return a.first < b.first; 
+}
+
+class Solution
+{
+    public:
+        vector<int> countSmaller(vector<int> &nums)
+        {
+            int len = nums.size();
+            vector<int> res(len, 0);
+            vector<int> index;
+            for(int i = 0; i < len; ++i)
+                index.push_back(i);
+            vector<int> numUpdate = nums;
+            vector<int> indexUpdate = index;
+            solve(res, nums, index, 0, len, numUpdate, indexUpdate);
+            return res;
+        }
+        void solve(vector<int> &res, vector<int> &nums, vector<int> &index, int start, int end, vector<int> &numUpdate, vector<int> &indexUpdate)
+        {
+            if(end - start <= 1)
+                return;
+            int mid = (start + end) / 2;
+            solve(res, nums, index, mid, end, numUpdate, indexUpdate);
+            solve(res, nums, index, start, mid, numUpdate, indexUpdate);
+            int r = mid;
+            int t = mid;
+            int s = start;
+            for(int l = start; l < mid; ++l)
+            {
+                while(nums[l] > nums[r] && r < end)
+                    r++;
+                while(t < end && nums[t] <= nums[l])
+                {
+                    numUpdate[s] = nums[t];
+                    indexUpdate[s++] = index[t++];
+                }
+                numUpdate[s] = nums[l];
+                indexUpdate[s++] = index[l];
+                res[index[l]] += r - mid;
+            }
+            for(int i = start; i < end; ++i)
+            {
+                nums[i] = numUpdate[i];
+                index[i] = indexUpdate[i];
+            }
+        }
+};
+
+
+
 int main()
 {
     return 0;
