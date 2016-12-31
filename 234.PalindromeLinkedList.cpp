d5625afd46c0070b5fe2cf9505fd170732ad0c74
zuilehongdou <15120101@bjtu.edu.cn>
2016-12-31 16:33:50 +0800

    update pro234 solution

--- 234.PalindromeLinkedList.cpp
+++ 234.PalindromeLinkedList.cpp
@@ -11,40 +11,37 @@ class Solution
 public:
     bool isPalindrome(ListNode* head) 
     {
-    	if(head == NULL || head->next == NULL)
-    		return true;
-    	ListNode *slow = head;
-    	ListNode *fast = head;
-    	while(fast->next && fast->next->next)
-    	{
-    		slow = slow->next;
-    		fast = fast->next->next;
-    	}
-    	ListNode *pre = slow->next;
-    	ListNode *cur = pre->next;
-    	slow->next = NULL;
-    	pre->next = NULL;
-    	if(cur == NULL)
-    		cur = pre;
-    	else
-    	{
-	    	while(cur->next)
-	    	{
-	    		ListNode *temp = cur->next;
-	    		cur->next = pre;
-	    		pre = cur;
-	    		cur = temp;
-	    	}
-	    	cur->next = pre;
-	    }
-    	ListNode *p = head;
-    	while(cur && p)
-    	{
-    		if(cur->val != p->val)
-    			return false;
-    		cur = cur->next;
-    		p = p->next;
-    	}
-    	return true;
+        if(head == NULL || head->next == NULL)
+            return true;
+        ListNode *slow = head;
+        ListNode *fast = head;
+        while(fast->next && fast->next->next)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        
+        ListNode *pre = slow->next;
+        ListNode *cur = pre->next;
+        slow->next = NULL;
+        pre->next = NULL;
+        
+        while(cur)
+        {
+            ListNode *temp = cur->next;
+            cur->next = pre;
+            pre = cur;
+            cur = temp;
+        }
+        ListNode *p = head;
+        
+        while(pre && p)
+        {
+            if(pre->val != p->val)
+                return false;
+            pre = pre->next;
+            p = p->next;
+        }
+        return true;
     }
 };
