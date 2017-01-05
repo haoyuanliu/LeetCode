72b4a349a43cfbfa1b7e1803c8054ebca3a0cde4
zuilehongdou <15120101@bjtu.edu.cn>
2017-01-05 13:49:45 +0800

    add pro437 solution

---
+++ 437.PathSumIII.cpp
@@ -0,0 +1,18 @@
+class Solution 
+{
+public:
+    int pathSum(TreeNode* root, int sum) 
+    {
+    	if(root == NULL)
+    		return 0;
+        return solve(root, sum, 0) + pathSum(root->left, sum) + pathSum(root->right, sum);
+    }
+    int solve(TreeNode* root, int target, int sum)
+    {
+    	if(root == NULL)
+    		return 0;
+    	if(sum + root->val == target)
+    		return 1 + solve(root->left, 0, 0) + solve(root->right, 0, 0);
+    	return solve(root->left, target, sum+root->val) + solve(root->right, target, sum+root->val);
+    }
+};
