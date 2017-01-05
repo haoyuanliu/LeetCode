3a4b9d69d5e4dd5c912ea4f5c415c62790c62d45
zuilehongdou <15120101@bjtu.edu.cn>
2017-01-05 10:35:49 +0800

    add pro104 solution

--- 104.MaximumDepthOfBinaryTree.cpp
+++ 104.MaximumDepthOfBinaryTree.cpp
@@ -1,24 +1,10 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
 class Solution 
 {
 public:
     int maxDepth(TreeNode* root) 
     {
-        return getDepth(root);
-    }
-
-    int getDepth(TreeNode* root)
-    {
-    	if(root)
-    		return 1 + max(getDepth(root->left), getDepth(root->right));
-    	return 0;
+        if(root)
+            return 1 + max(maxDepth(root->left), maxDepth(root->right));
+        return 0;
     }
 };
