d356d615a0a1ec4d95b47caeef28ef3fcfc6eac1
zuilehongdou <15120101@bjtu.edu.cn>
2017-01-10 16:15:08 +0800

    update pro117 solution

--- 117.PopulatingNextRightPointersInEachNodeII.cpp
+++ 117.PopulatingNextRightPointersInEachNodeII.cpp
@@ -1,3 +1,4 @@
+Solution I
 class Solution 
 {
 public:
@@ -24,6 +25,45 @@ public:
                 }               
             }
             p->next = NULL;
+        }
+    }
+};
+
+//Solution II
+class Solution 
+{
+public:
+    void connect(TreeLinkNode *root) 
+    {
+        if(root == NULL)
+            return;
+        TreeLinkNode* head = NULL;
+        TreeLinkNode* pre = NULL;
+        TreeLinkNode* cur = root;
+        while(cur)
+        {
+            while(cur)
+            {
+                if(cur->left)
+                {
+                    if(!head)
+                        head = cur->left;
+                    else
+                        pre->next = cur->left;
+                    pre = cur->left;
+                }
+                if(cur->right)
+                {
+                    if(!head)
+                        head = cur->right;
+                    else
+                        pre->next = cur->right;
+                    pre = cur->right;
+                }
+                cur = cur->next;
+            }
+            cur = head;
+            head = pre = NULL;
         }
     }
 };
