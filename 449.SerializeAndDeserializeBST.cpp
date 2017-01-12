68a0de77d4efe047717ea6a9d4bfcf10ba2d94b9
zuilehongdou <15120101@bjtu.edu.cn>
2017-01-12 10:43:28 +0800

    add pro449 solution

--- 449.SerializeAndDeserializeBST.cpp
+++ 449.SerializeAndDeserializeBST.cpp
@@ -13,14 +13,16 @@ public:
     // Decodes your encoded data to tree.
     TreeNode* deserialize(string data) 
     {
+        if(data == "") return NULL;
         istringstream in(data);
+        return myDeserialize(in);
     }
 private:
 	void mySerialize(TreeNode* root, ostringstream &out)
 	{
 		if(root == NULL) return;
-		mySerialize(root->left, out);
 		out << root->val << " ";
+		mySerialize(root->left, out);
 		mySerialize(root->right, out);
 	}
 	TreeNode* myDeserialize(istringstream &in)
@@ -28,9 +30,25 @@ private:
 		string val;
 		in >> val;
 		TreeNode *root = new TreeNode(stoi(val));
-		while(!in.empty())
+		while(in >> val)
+			buildTree(root, stoi(val));
+		return root;
+	}
+	void buildTree(TreeNode* root, int n)
+	{
+		if(root->val > n)
 		{
-			
+			if(root->left == NULL)
+				root->left = new TreeNode(n);
+			else
+				buildTree(root->left, n);
+		}
+		else
+		{
+			if(root->right == NULL)
+				root->right = new TreeNode(n);
+			else
+				buildTree(root->right, n);
 		}
 	}
 };
