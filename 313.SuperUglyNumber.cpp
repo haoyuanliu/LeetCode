c251249775c4bbdd8ba8ea7e45d4e0520c81644b
15120101@bjtu.edu.cn <15120101@bjtu.edu.cn>
2017-02-04 11:47:33 +0800

    add 313 solution but not ac

--- 313.SuperUglyNumber.cpp
+++ 313.SuperUglyNumber.cpp
@@ -1,24 +1,37 @@
+//Solution I TLE
 class Solution
 {
 public:
     int nthSuperUglyNumber(int n, vector<int>& primes)
     {
         int res = 1;
-        if(n == 1)
-            return res;
+        if(n == 1) return res;
         priority_queue<int> heap;
         for(int i = 0; i < primes.size(); ++i)
             heap.push(-1 * primes[i]);
         int t = n - 1;
         while(t--)
         {
-            int temp = heap.top();
+            if(heap.empty()) return res;
+            while(heap.top() == res)
+                heap.pop();
+            res = heap.top();
             heap.pop();
-            res = -1 * temp;
             for(int i = 0; i < primes.size(); ++i)
-                heap.push(temp - primes[i]);
-
+                heap.push(res * primes[i]);
         }
-        return res;
+        return -1 * res;
+    }
+};
+
+
+//Solution II
+class Solution
+{
+public:
+    int nthSuperUglyNumber(int n, vector<int>& primes)
+    {
+        int res = 1;
+        if(n == 1) return res;
     }
 };
