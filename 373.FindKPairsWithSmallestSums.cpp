2a005879d6db3d031b4ff44bf1025d68636eb706
15120101@bjtu.edu.cn <15120101@bjtu.edu.cn>
2017-02-06 15:16:14 +0800

    add pro373 solution

--- 373.FindKPairsWithSmallestSums.cpp
+++ 373.FindKPairsWithSmallestSums.cpp
@@ -1,24 +1,20 @@
 class Solution
 {
-    struct Pairs
-    {
-        int a, b;
-        int sum;
-        Pairs(int a_, int b_) : a(a_), b(b_), sum(a + b) {}
-        bool operator<(const Pairs ref) { return sum < ref.sum; }
-    };
 public:
     vector<pair<int, int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k)
     {
         vector<pair<int, int>> res;
-        priority_queue<Pairs> que;
+        priority_queue<pair<int, pair<int, int>>> que;
         for(int i = 0; i < nums1.size(); ++i)
         {
-            que.push(Pairs(nums1[i], nums2[i]));
+            for(int j = 0; j < nums2.size(); ++j)
+            {
+                que.push(make_pair(-nums1[i]-nums2[j], make_pair(nums1[i], nums2[j])));
+            }
         }
         for(int i = 0; i < k && !que.empty(); ++i)
         {
-            res.push_back(make_pair(que.top().a, que.top().b));
+            res.push_back(make_pair(que.top().second.first, que.top().second.second));
             que.pop();
         }
         return res;
