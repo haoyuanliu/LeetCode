6ca2e3e724b82f4f6a4e7782fdf48daa6b8e3b22
15120101@bjtu.edu.cn <15120101@bjtu.edu.cn>
2017-02-07 21:37:55 +0800

    add pro407 solution but not ac

---
+++ 407.TrappingRainWaterII.cpp
@@ -0,0 +1,10 @@
+class Solution
+{
+public:
+    int trapRainWater(vector<vector<int>>& heightMap)
+    {
+        int res = 0;
+        if(heightMap.size() <= 2)
+            return res;
+    }
+};
