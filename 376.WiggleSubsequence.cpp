564eaf24af645555c6943c5480b219c1300a5353
zuilehongdou <15120101@bjtu.edu.cn>
2017-02-28 10:16:00 +0800

    update pro376 solution

--- 376.WiggleSubsequence.cpp
+++ 376.WiggleSubsequence.cpp
@@ -4,15 +4,15 @@ public:
     int wiggleMaxLength(vector<int>& nums)
     {
         int len = nums.size();
-        int po = 1;
-        int ne = 1;
+        int pos = 1;
+        int neg = 1;
         for(int i = 1; i < len; ++i)
         {
             if(nums[i] > nums[i-1])
-                po = ne + 1;
+                pos = neg + 1;
             else if(nums[i] < nums[i-1])
-                ne = po + 1;
+                neg = pos + 1;
         }
-        return min(len, max(ne, po));
+        return min(len, max(neg, pos));
     }
 };
