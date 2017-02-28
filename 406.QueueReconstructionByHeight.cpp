ca7dc76766dc790cbd65f46d16b665de56ef86e4
haoyuanliu <15120101@bjtu.edu.cn>
2017-02-28 13:01:59 +0800

    add pro 406 solution

--- 406.QueueReconstructionByHeight.cpp
+++ 406.QueueReconstructionByHeight.cpp
@@ -4,25 +4,22 @@ public:
     vector<pair<int, int>> reconstructQueue(vector<pair<int, int>>& people)
     {
         int len = people.size();
-        vector<pair<int, int>> res(len, {0, 0});
-        vector<int> visited(len, 0);
-
+        vector<pair<int, int>> res(len, {-1, -1});
         sort(people.begin(), people.end());
-
+        int count, i;
         for(const auto p : people)
         {
-            int count = 0;
-            int i = 0;
-            for(int i = 0; i < len; ++i)
+            count = 0;
+            i = 0;
+            for(i = 0; i < len; ++i)
             {
-                if(visited[i] == 0)
+                if(res[i].first == -1 || res[i].first >= p.first)
                     count++;
                 if(count > p.second)
                     break;
             }
             res[i].first = p.first;
             res[i].second = p.second;
-            visited[i] = 1;
         }
         return res;
     }
