e9f14367f38717994726dfc4f63cca28519edf35
zuilehongdou <15120101@bjtu.edu.cn>
2017-02-28 16:42:06 +0800

    update pro455 solution

--- 455.AssignCookies.cpp
+++ 455.AssignCookies.cpp
@@ -22,23 +22,3 @@ public:
         return res;
     }
 };
-
-class Solution
-{
-public:
-    int findContentChildren(vector<int>& g, vector<int>& s)
-    {
-        int res = 0;
-        sort(g.begin(), g.end());
-        sort(s.begin(), s.end());
-
-        int x = g.size() - 1;
-        int y = s.size() - 1;
-        while(x >= 0 && y >= 0)
-        {
-            if(g[x] > s[y]) x--;
-            else if(g[x--] <= s[y--]) res++;
-        }
-        return res;
-    }
-};
