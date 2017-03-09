5b1411be70240f4d47ea3fd18ade487f38419a2e
haoyuanliu <15120101@bjtu.edu.cn>
2017-03-09 22:24:39 +0800

    update pro4 solution

--- 4.MedianOfTwoSortedArrays.cpp
+++ 4.MedianOfTwoSortedArrays.cpp
@@ -1,53 +1,50 @@
-#include <iostream>
-#include <vector>
-using namespace std;
-class Solution
-  {
-    public:
+class Solution {
+public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2)
     {
-          vector<double> res;
-          int la = nums1.size();
-          int lb = nums2.size();
-          int mid = (la + lb) / 2;
-          int i = 0;
-          int j = 0;
-          double ans;
-          while(i < la && j < lb)
-          {
-              if(nums1[i] > nums2[j])
-              {
-                  res.push_back(nums2[j]);
-                  j++;
-              }
-              else
-              {
-                  res.push_back(nums1[i]);
-                  i++;
-              }
-          }
-          while(i < la)
-          {
-              res.push_back(nums1[i]);
-              i++;
-          }
-          while(j < lb)
-          {
-              res.push_back(nums2[j]);
-              j++;
-          }
-          if((la+lb) % 2)
-              ans = res[mid];
-          else
-            ans = (res[mid]+res[mid-1])/2;
-          return ans;
+        double res = 0;
+        int len1 = nums1.size();
+        int len2 = nums2.size();
+        int mid = (len1 + len2 + 1) / 2;
+        int count, i, j;
+        double pre, cur;
+        count = i = j = 0;
+        while(i < nums1.size() && j < nums2.size())
+        {
+            count++;
+            if(count == mid)
+                pre = min(nums1[i], nums2[j]);
+            else if(count == mid+1)
+                cur = min(nums1[i], nums2[j]);
+            else if(count > mid+1) break;
+            if(nums1[i] < nums2[j])
+                i++;
+            else
+                j++;
+        }
+        while(i < nums1.size())
+        {
+            count++;
+            if(count == mid)
+                pre = nums1[i];
+            else if(count == mid+1)
+                cur = nums1[i];
+            else if(count > mid+1) break;
+            i++;
+        }
+        while(j < nums2.size())
+        {
+            count++;
+            if(count == mid)
+                pre = nums2[j];
+            else if(count == mid+1)
+                cur = nums2[j];
+            else if(count > mid+1) break;
+            j++;
+        }
+        if((len1 + len2) % 2)
+            return pre;
+        else
+            return (pre + cur) / 2;
     }
 };
-int main()
-{
-  vector<int> a = {1, 3};
-  vector<int> b = {2};
-  Solution res;
-  cout << res.findMedianSortedArrays(a, b) << endl;
-  return 0;
-}
